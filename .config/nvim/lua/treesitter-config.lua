2a4c5fc46ac497d92dde0ab70e7ca7988b3c8150
David Hotham <david.hotham@metaswitch.com>
2020-12-20 23:14:29 +0000

    put treesitter config in lua file

---
+++ .config/nvim/lua/treesitter-config.lua
@@ -0,0 +1,53 @@
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {
+    'bash',
+    'c',
+    'cpp',
+    'go',
+    'json',
+    'lua',
+    'python',
+    'rust',
+    'toml',
+    'yaml',
+    'query',
+  },
+  highlight = {
+    enable = true,
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@comment.outer",
+      },
+    },
+    move = {
+      enable = true,
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
+    },
+  },
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25,
+    persist_queries = false
+  },
+}
