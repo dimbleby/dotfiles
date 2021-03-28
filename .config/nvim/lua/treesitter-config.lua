679a9a7f0653d8e56a975bc80cb15733b9045634
David Hotham <david.hotham@metaswitch.com>
2021-03-28 00:57:29 +0000

    improved vim startup time

--- .config/nvim/lua/treesitter-config.lua
+++ .config/nvim/lua/treesitter-config.lua
@@ -32,10 +32,4 @@ require'nvim-treesitter.configs'.setup {
       },
     },
   },
-  playground = {
-    enable = true,
-    disable = {},
-    updatetime = 25,
-    persist_queries = false
-  },
 }
