2e5651974b9b036a2be8f03db2852578c32ccc34
David Hotham <david.hotham@metaswitch.com>
2021-08-25 01:26:08 +0100

    nvim-cmp

--- .config/nvim/lua/completion.lua
+++ .config/nvim/lua/completion.lua
@@ -1,65 +1,57 @@
-require'compe'.setup {
-  enabled = true,
-  autocomplete = true,
-  debug = false,
-  min_length = 2,
-  preselect = 'enable',
-  throttle_time = 80,
-  source_timeout = 200,
-  incomplete_delay = 400,
-  max_abbr_width = 100,
-  max_kind_width = 100,
-  max_menu_width = 100,
-  documentation = true,
-
-  source = {
-    vsnip = {priority = 40},
-    path = {priority = 30},
-    nvim_lsp = {priority = 20},
-    buffer = {priority = 10},
-  };
-}
-
 local t = function(str)
   return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
-local check_back_space = function()
-  local col = vim.fn.col('.') - 1
-  if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-    return true
-  else
-    return false
-  end
-end
+local cmp = require('cmp')
+cmp.setup {
+  completion = {
+    keyword_length = 2
+  },
 
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
-_G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-n>"
-  elseif vim.fn.call("vsnip#available", {1}) == 1 then
-    return t "<Plug>(vsnip-expand-or-jump)"
-  elseif check_back_space() then
-    return t "<Tab>"
-  else
-    return vim.fn['compe#complete']()
-  end
-end
-_G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-p>"
-  elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
-    return t "<Plug>(vsnip-jump-prev)"
-  else
-    return t "<S-Tab>"
-  end
-end
+  snippet = {
+    expand = function(args)
+      vim.fn['vsnip#anonymous'](args.body)
+    end
+  },
 
-local opts = {expr = true, silent = true}
-vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", opts)
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", opts)
-vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", opts)
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", opts)
-vim.api.nvim_set_keymap("i", "<CR>", "compe#confirm('<CR>')", opts)
+  mapping = {
+    ['<Tab>'] = cmp.mapping(function(fallback)
+        if vim.fn.pumvisible() == 1 then
+          vim.fn.feedkeys(t('<C-n>'), 'n')
+        elseif vim.fn['vsnip#available']() == 1 then
+          vim.fn.feedkeys(t('<Plug>(vsnip-expand-or-jump)'))
+        else
+          fallback()
+        end
+      end,
+      { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+        if vim.fn.pumvisible() == 1 then
+          vim.fn.feedkeys(t('<C-p>'), 'n')
+        elseif vim.fn['vsnip#available']() == 1 then
+          vim.fn.feedkeys(t('<Plug>(vsnip-jump-prev)'))
+        else
+          fallback()
+        end
+      end,
+      { 'i', 's' }),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    })
+  },
+
+  sources = {
+    { name = 'vsnip' },
+    { name = 'path' },
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+  },
+
+  sorting = {
+    priority_weight = 10
+  },
+}
