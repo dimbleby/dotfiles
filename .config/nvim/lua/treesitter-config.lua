9dc7f0e2ab84862a3dc0652b9e3d0ba669c1d678
David Hotham <david.hotham@metaswitch.com>
2021-12-01 11:59:18 +0000

    try incremental selection

--- .config/nvim/lua/treesitter-config.lua
+++ .config/nvim/lua/treesitter-config.lua
@@ -3,6 +3,15 @@ require('nvim-treesitter.configs').setup {
   highlight = {
     enable = true,
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<CR>',
+      scope_incremental = '<CR>',
+      node_incremental = '<TAB>',
+      node_decremental = '<S-TAB>',
+    },
+  },
   textobjects = {
     select = {
       enable = true,
