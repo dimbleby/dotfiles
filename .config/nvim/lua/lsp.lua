78c9b3c5161fcb12e057e2361087720237b61f4b
David Hotham <david.hotham@blueyonder.co.uk>
2022-09-11 20:41:57 +0100

    deprecations

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,23 +1,20 @@
 vim.lsp.set_log_level("OFF")
 
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-  buf_set_option("tagfunc", "v:lua.vim.lsp.tagfunc")
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  vim.api.nvim_buf_set_option(bufnr, 'tagfunc', 'v:lua.vim.lsp.tagfunc')
 
   -- Mappings.
-  local opts = {noremap = true, silent = true}
-  buf_set_keymap('n', '<C-]>', '<Cmd>Telescope lsp_definitions<CR>', opts)
-  buf_set_keymap('n', '<LocalLeader>t', '<Cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', '<LocalLeader>i', '<Cmd>Telescope lsp_implementations<CR>', opts)
-  buf_set_keymap('n', '<LocalLeader>n', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<LocalLeader>r', '<Cmd>Telescope lsp_references<CR>', opts)
-  buf_set_keymap('n', '<LocalLeader>a', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('v', '<LocalLeader>a', '<Cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
-  buf_set_keymap('n', '<LocalLeader>s', '<Cmd>Telescope lsp_dynamic_workspace_symbols<CR>', opts)
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  local telescope_builtin = require('telescope.builtin')
+  vim.keymap.set('n', '<C-]>', telescope_builtin.lsp_definitions, opts)
+  vim.keymap.set('n', '<LocalLeader>t', vim.lsp.buf.type_definition, opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', '<LocalLeader>i', telescope_builtin.lsp_implementations, opts)
+  vim.keymap.set('n', '<LocalLeader>n', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', '<LocalLeader>r', telescope_builtin.lsp_references, opts)
+  vim.keymap.set({'n', 'v'}, '<LocalLeader>a', vim.lsp.buf.code_action, opts)
+  vim.keymap.set('n', '<LocalLeader>s', telescope_builtin.lsp_dynamic_workspace_symbols, opts)
 end
 
 local lspconfig = require('lspconfig')
