b7f0b94050c2e763bb2e680dffd696cd8efea2f3
David Hotham <david.hotham@blueyonder.co.uk>
2023-02-16 19:47:33 +0000

    teensy refactor

--- .config/nvim/lua/utils/linting.lua
+++ .config/nvim/lua/utils/linting.lua
@@ -1,16 +1,18 @@
 local M = {}
 
+local lint = function()
+  require('lint').try_lint(nil, { ignore_errors = true })
+end
+
 function M.set_up_linting()
   vim.api.nvim_create_augroup('Linting', { clear = false })
   vim.api.nvim_clear_autocmds({ buffer = 0, group = 'Linting' })
   vim.api.nvim_create_autocmd('BufWritePost', {
     group = 'Linting',
     buffer = 0,
-    callback = function()
-      require('lint').try_lint(nil, { ignore_errors = true })
-    end,
+    callback = lint,
   })
-  require('lint').try_lint(nil, { ignore_errors = true })
+  lint()
 end
 
 return M
