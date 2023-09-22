eaca11302ff410ad22568723773d4528bb453dc2
David Hotham <david.hotham@microsoft.com>
2023-09-22 11:14:10 +0100

    allow ruff formatting

--- .config/nvim/after/ftplugin/python.lua
+++ .config/nvim/after/ftplugin/python.lua
@@ -4,12 +4,13 @@ vim.opt_local.foldmethod = 'indent'
 local formatprgs = {}
 if vim.fn.executable('ruff') == 1 then
   table.insert(formatprgs, 'ruff --quiet --fix --stdin-filename % -')
-end
-if vim.fn.executable('isort') == 1 then
+elseif vim.fn.executable('isort') == 1 then
   table.insert(formatprgs, 'isort --quiet -')
 end
 if vim.fn.executable('black') == 1 then
   table.insert(formatprgs, 'black --quiet -')
+elseif vim.fn.executable('ruff') == 1 then
+  table.insert(formatprgs, 'ruff format --quiet --stdin-filename % -')
 end
 if next(formatprgs) ~= nil then
   vim.opt_local.formatprg = table.concat(formatprgs, '|')
