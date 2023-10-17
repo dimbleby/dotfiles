285f06cc59d5369bb49720c968a91f148d4b13a4
David Hotham <david.hotham@blueyonder.co.uk>
2023-10-17 21:25:02 +0100

    consistent {yaml,yml}

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -81,11 +81,11 @@ lspconfig.yamlls.setup({
     yaml = {
       schemas = {
         ['https://json.schemastore.org/clang-format.json'] = '/.clang-format',
-        ['https://json.schemastore.org/dependabot-2.0.json'] = '/.github/dependabot.ya?ml',
+        ['https://json.schemastore.org/dependabot-2.0.json'] = '/.github/dependabot.{yaml,yml}',
         ['https://json.schemastore.org/github-workflow.json'] = '/.github/workflows/*',
-        ['https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json'] = '/.gitlab-ci.ya?ml',
-        ['https://json.schemastore.org/pre-commit-config.json'] = '/.pre-commit-config.ya?ml',
-        ['https://json.schemastore.org/pre-commit-hooks.json'] = '/.pre-commit-hooks.ya?ml',
+        ['https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json'] = '/.gitlab-ci.{yaml,yml}',
+        ['https://json.schemastore.org/pre-commit-config.json'] = '/.pre-commit-config.{yaml,yml}',
+        ['https://json.schemastore.org/pre-commit-hooks.json'] = '/.pre-commit-hooks.{yaml,yml}',
       },
     },
   },
