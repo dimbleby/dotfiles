125be23b11617ca714afb0376dff1cd982045e5c
David Hotham <david.hotham@blueyonder.co.uk>
2025-02-22 20:43:47 +0000

    rust edition 2024

--- .config/nvim/after/ftplugin/rust.lua
+++ .config/nvim/after/ftplugin/rust.lua
@@ -1,3 +1,3 @@
 if vim.fn.executable('rustfmt') == 1 then
-  vim.opt_local.formatprg = 'rustfmt --edition 2021 --color never --emit stdout'
+  vim.opt_local.formatprg = 'rustfmt --edition 2024 --color never --emit stdout'
 end
