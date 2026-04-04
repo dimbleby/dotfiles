c3115440545f8d13a65471ba17168d130e8dc08b
David Hotham <david.hotham@blueyonder.co.uk>
2026-04-04 19:52:28 +0100

    remove redundant (default) neovim options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,42 +1,22 @@
-vim.opt.autoread = true
-vim.opt.background = 'dark'
-vim.opt.complete:remove('i')
 vim.opt.completeopt = 'menuone,noselect'
 vim.opt.dictionary = '/usr/share/dict/words'
-vim.opt.display:append({ 'lastline' })
 vim.opt.diffopt:append({ 'algorithm:histogram,indent-heuristic,linematch:60,vertical' })
 vim.opt.foldlevelstart = 99
 vim.opt.hidden = false
-vim.opt.history = 10000
-vim.opt.inccommand = 'nosplit'
-vim.opt.incsearch = true
 vim.opt.jumpoptions = 'stack'
-vim.opt.laststatus = 2
 vim.opt.lazyredraw = true
 vim.opt.list = true
-vim.opt.listchars = {
-  tab = '> ',
-  trail = '-',
-  extends = '>',
-  precedes = '<',
-  nbsp = '+',
-}
+vim.opt.listchars:append({ extends = '>', precedes = '<' })
 vim.opt.mouse = ''
 vim.opt.modeline = false
-vim.opt.nrformats:remove('octal')
 vim.opt.scrolloff = 1
 vim.opt.sidescrolloff = 5
 vim.opt.sidescroll = 1
-vim.opt.sessionoptions:remove('options')
 vim.opt.shortmess:append('c')
-vim.opt.showcmd = true
 vim.opt.showmode = false
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-vim.opt.tabpagemax = 50
-vim.opt.wildmenu = true
 vim.opt.expandtab = true
-vim.opt.smarttab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 0
 vim.opt.softtabstop = -1
