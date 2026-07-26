735b206f2e4514b02b902cea063e2a72f832d095
David Hotham <david.hotham@microsoft.com>
2026-07-26 15:30:24 +0100

    fix filename expansion in toml formatting

--- .config/nvim/after/ftplugin/toml.lua
+++ .config/nvim/after/ftplugin/toml.lua
@@ -1,4 +1,4 @@
 if vim.fn.executable('tombi') == 1 then
-  local path = vim.fn.expand('%:p:h')
+  local path = vim.fn.expand('%:p')
   vim.opt_local.formatprg = 'tombi format --quiet --stdin-filename ' .. path .. ' -'
 end
